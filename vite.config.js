3bba0efeeb5922c4548ee4594d1d87439290da11
midhunmahesh2001 <midhunmahesh2001@gmail.com>
2024-09-29 14:30:28 +0530

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/midhun__mahesh/',
-  plugins: [react()],
+  base: '/midhun__mahesh/',  // Use your exact repository name
+  plugins: [react()]
 })

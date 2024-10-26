e434a1708a9ac024b783a2eaa9934dc7ea65fc76
midhunmahesh2001 <midhunmahesh2001@gmail.com>
2024-10-26 19:41:45 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Certificates from './Components/Certificates/Certificates';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Nav from './Components/Navbar/Nav';
+import Projects from './Components/Projects/Projects';
+import ContactForm from './Components/ContactForm/ContactForm';
 
 function App() {
-
   return (
-   <>
-  <h1>hii im midhun</h1>
-   </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/midhun__mahesh/" element={
+          <>
+            <Nav />
+            <Header />
+            <Projects />
+            <Certificates />
+            <Footer />
+          </>
+        } />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
